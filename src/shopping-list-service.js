6895222d63b835cd85fdc00bbd627024f7ef4721
keithfilegar <keith.filegar@gmail.com>
2021-02-16 09:53:29 -0800

    build shoppingListService and associated CRUD tests

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,5 +1,33 @@
 const ShoppingListService = {
-
+    getShoppingList(knex) {
+        return knex.select('*').from('shopping_list')
+    },
+    insertListItem(knex, newItem) {
+        return knex
+            .insert(newItem)
+            .into('shopping_list')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    getItemById(knex, id) {
+        return knex
+            .from('shopping_list')
+            .select('*')
+            .where('id', id)
+            .first()
+    },
+    deleteListItem(knex, id) {
+        return knex('shopping_list')
+            .where({id})
+            .delete()
+    },
+    updateListItem(knex, id, newListItemFields) {
+        return knex('shopping_list')
+            .where({id})
+            .update(newListItemFields)
+    }
 }
 
 module.exports = ShoppingListService
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -8,26 +8,26 @@ describe(`Shopping list service object`, function() {
         {
             id: 1,
             name: "Instant Noods",
-            price: 2.67,
+            price: "2.67",
             date_added: new Date('1919-12-22T16:28:32.615Z'),
             checked: true,
-            category: Snack
+            category: 'Snack'
         },
         {
             id: 2,
             name: "Count Chocula",
-            price: 4.99,
+            price: "4.99",
             date_added: new Date('1989-12-22T16:28:32.615Z'),
             checked: false,
-            category: Breakfast
+            category: 'Breakfast'
         },
         {
             id: 3,
             name: "Franken Berry",
-            price: 4.16,
+            price: "4.16",
             date_added: new Date('1969-12-22T16:28:32.615Z'),
             checked: true,
-            category: Breakfast
+            category: 'Breakfast'
         }
     ]
 
@@ -52,7 +52,88 @@ describe(`Shopping list service object`, function() {
         })
 
         it(`getShoppingList() resolves entire shopping list from 'shopping_list' table`, () => {
-            
+            return ShoppingListService.getShoppingList(db)
+                .then(actual => {
+                    expect(actual).to.eql(testList.map(item => ({
+                        ...item,
+                        date_added: new Date(item.date_added)
+                    })))
+                })
+        })
+
+        it(`getItemById() resolves an article by id from 'shopping_list' table`, () => {
+            const thirdItemId = 3
+            const thirdListItem = testList[thirdItemId - 1]
+            return ShoppingListService.getItemById(db, thirdItemId)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: thirdItemId,
+                        name: thirdListItem.name,
+                        price: thirdListItem.price,
+                        date_added: thirdListItem.date_added,
+                        checked: thirdListItem.checked,
+                        category: thirdListItem.category
+                    })
+                })
+        })
+
+        it(`deleteListItem() removes list item by id from 'shopping_list' table`, () => {
+            const itemId = 3
+            return ShoppingListService.deleteListItem(db, itemId) 
+                .then(() => ShoppingListService.getShoppingList(db))
+                .then(updatedList => {
+                    const expected = testList.filter(listItem => listItem.id !== itemId)
+                    expect(updatedList).to.eql(expected)
+                })
+        })
+
+        it(`updateListItem() updates a list item from 'shopping_list' table`, () => {
+            const idToUpdate = 3
+            const newItemData = {
+                name: 'Boo Berry',
+                price: '3.80',
+                date_added: new Date('1979-12-22T16:28:32.615Z'),
+                checked: false,
+                category: 'Breakfast'
+            }
+            return ShoppingListService.updateListItem(db, idToUpdate, newItemData)
+                .then(() => ShoppingListService.getItemById(db, idToUpdate))
+                .then(listItem => {
+                    expect(listItem).to.eql({
+                        id: idToUpdate,
+                        ...newItemData
+                    })
+                })
+        })
+    })
+
+    context(`Given 'shopping_list' has no data`, () => {
+        it(`getShoppingList() resolves as an empty array`, () => {
+            return ShoppingListService.getShoppingList(db)
+                .then(actual => {
+                    expect(actual).to.eql([])
+                })
+        })
+
+        it(`insertListItem() inserts a new list item and resolves the new article with an 'id'`, () => {
+            const newItem = {
+                name: 'New item',
+                price: "6.66",
+                date_added: new Date('1999-12-22T16:28:32.615Z'),
+                checked: true,
+                category: 'Main'
+            }
+            return ShoppingListService.insertListItem(db, newItem)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: 1,
+                        name: newItem.name,
+                        price: newItem.price,
+                        date_added: newItem.date_added,
+                        checked: newItem.checked,
+                        category: newItem.category
+                    })
+                })
         })
     })
 })
